0600106a20c3cf5156aacc9dff457b29499f6816
wzxmt <44249535+wzxmt@users.noreply.github.com>
2022-05-13 17:39:12 +0800

    Update docker_build.sh

--- k8s/docker_build.sh
+++ k8s/docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker_registry=registry.wzxmt.com
+image_tag=latest
 # 存储登录Harbor认证信息
 kubectl create secret docker-registry registry-pull-secret --docker-server=$docker_registry --docker-username=admin --docker-password=admin -n ms
 
@@ -19,7 +20,7 @@ for service in $service_list; do
       cd ${service}-biz
    fi
    service=${service%-*}
-   image_name=$docker_registry/microservice/${service}:$(date +%F-%H-%M-%S)
+   image_name=$docker_registry/microservice/${service}:${image_tag}
    docker build -t ${image_name} .
    docker push ${image_name} 
    # 修改yaml中镜像地址为新推送的，并apply
